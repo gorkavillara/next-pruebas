8f0a4389185065482c348d0b3f0c149c399831de
Gorka <gorkavillara@gmail.com>
2022-05-02 12:18:29 +0200

    Primer commit de pruebas

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { logger } from "../../controllers/winston"
+
 export default function handler(req, res) {
+  console.log("Api")
+  logger.log("error", "Esto es un error de pruebas")
   res.status(200).json({ name: 'John Doe' })
 }
